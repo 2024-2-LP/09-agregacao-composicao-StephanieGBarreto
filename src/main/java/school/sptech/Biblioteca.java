9b606036a23b9ee7e26d67fba1138d476e0baf9a
StephanieGBarreto <stephanie.gbarreto@sptech.school>
2024-10-23 19:27:08 -0300

    Atividade 9 agregação e composição

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,33 @@
 package school.sptech;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Biblioteca {
+    private List<Livro> livros;
+
+    public Biblioteca() {
+        this.livros = new ArrayList<>();
+    }
+
+    public void adicionarLivro(Livro livro) {
+        this.livros.add(livro);
+    }
+
+    public void removerLivro(String titulo) {
+        livros.removeIf(livro -> livro.getTitulo().equals(titulo));
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        for (Livro livro : livros) {
+            if (livro.getTitulo().equals(titulo)) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public List<Livro> getLivros() {
+        return livros;
+    }
 }
